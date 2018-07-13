edb94997d955b5e591dd1969188e11ad8d479055
Brandon <Brandon.Jewell@LibertyMutual.com>
2018-07-13 14:38:20 -0400

    Added GradeBook Type

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -13,6 +13,7 @@ namespace GradeBook.GradeBooks
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+        public Type GradeBookType;
 
         public BaseGradeBook(string name)
         {
